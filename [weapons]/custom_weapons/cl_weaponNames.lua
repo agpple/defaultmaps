c58543cb9119a9e06ce186ee7c75c50f5ffe5f2e
Xiaozhong <zxas0983000242@gmail.com>
2023-09-30 21:19:43 +0800

    1.0

---
+++ [weapons]/custom_weapons/cl_weaponNames.lua
@@ -0,0 +1,10 @@
+-- This Package Leaked By Tc Leaks.
+-- For More Leaks Join Our Discord Server: https://discord.gg/tc-leaks / https://discord.gg/u7fNWZPxRB
+-- LEAKED BY TC LEAKS
+
+AddTextEntry("WEAPON_HUNTINGRIFLE", "Hunting Rifle")
+
+
+-- This Package Leaked By Tc Leaks.
+-- For More Leaks Join Our Discord Server: https://discord.gg/tc-leaks / https://discord.gg/u7fNWZPxRB
+-- LEAKED BY TC LEAKS
